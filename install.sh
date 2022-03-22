be75c84458ff7d418013bbac710d2295abd672a2
Nix4Lyfe <16310251+nix4lyfe@users.noreply.github.com>
2022-03-22 12:03:28 -0500

    added nvim to install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 rm scripts/cputemp scripts/newhostname
 sed -i '/alias laptopmode="$HOME/scripts/laptopmode.sh"/d' scripts/aliases
 sed -i '/alias cputemp="$HOME/scripts/cputemp.sh"/d' scripts/aliases
-cp -R ./{scripts,.zshrc,.zhistory} ~$HOME/
+cp -R ./{scripts,.zshrc,.zhistory,.config,.local} ~$HOME/
 cd $HOME
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 rm -rf zsh
