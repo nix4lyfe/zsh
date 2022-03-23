37a036150cde2599ca2b824584056bda76263aa1
nix4lyfe <16310251+nix4lyfe@users.noreply.github.com>
2022-03-23 00:09:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sed -i '/alias laptopmode="$HOME/scripts/laptopmode.sh"/d' scripts/aliases
 sed -i '/alias cputemp="$HOME/scripts/cputemp.sh"/d' scripts/aliases
 cp -R ./{scripts,.zshrc,.zhistory,.config,.local} $HOME/
 cd $HOME
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 rm -rf zsh
