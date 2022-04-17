ba2f174a5ef0f3b8e26295117965f6af0c1f11f0
nix4lyfe <16310251+nix4lyfe@users.noreply.github.com>
2022-04-17 14:04:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
-!#/bin/bash
-rm scripts/cputemp scripts/newhostname
-sed -i '/alias laptopmode="$HOME/scripts/laptopmode.sh"/d' scripts/aliases
-sed -i '/alias cputemp="$HOME/scripts/cputemp.sh"/d' scripts/aliases
+#!/bin/bash
+cd $HOME/zsh
+rm scripts/newhostname
+printf "%s " "Would you like to delete cputemp? Note this only works on RaspberryPi <y/N>"
+read prompt
+if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
+then
+rm scripts/cputemp
+sed -i'' -e '/alias cputemp="$HOME\/scripts\/cputemp.sh"/d' scripts/aliases
+fi
+printf "%s " "Would you like to delete laptopmode? <y/N>"
+read prompt
+if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
+then
+rm scripts/laptopmode
+sed -i'' -e '/alias laptopmode="$HOME\/scripts\/laptopmode.sh"/d' scripts/aliases
+fi
 cp -R ./{scripts,.zshrc,.zhistory,.config,.local} $HOME/
-cd $HOME
+cd $HOME/
 curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 rm -rf zsh
