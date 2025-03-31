d5e595e42fc400facde268b45a6c1f14fc9e7889
nix4lyfe <16310251+nix4lyfe@users.noreply.github.com>
2025-03-31 12:12:55 -0500

    Update and rename setopt to setopt.zsh

--- scripts/startup-scripts/setopt.zsh
+++ scripts/startup-scripts/setopt.zsh
@@ -1,3 +1,4 @@
+bindkey -e
 setopt INC_APPEND_HISTORY #Add each line to the history as you type
 setopt SHARE_HISTORY
 setopt EXTENDED_HISTORY #ADD TIME AND DATE TO HISTORY
@@ -5,11 +6,14 @@ setopt HIST_FIND_NO_DUPS #Dup history are not printed even if there
 setopt HIST_IGNORE_ALL_DUPS #Ignore duplicates in the History
 setopt HIST_SAVE_NO_DUPS #Don't save duplicates
 setopt HIST_IGNORE_SPACE #Ignore spaces in the History
+setopt HIST_REDUCE_BLANKS #Removes blank lines from history
 setopt INTERACTIVE_COMMENTS #Add comments right on the command line
+setopt HIST_EXPIRE_DUPS_FIRST #Expire duplicates first
 setopt CORRECT #Autocorrect
+setopt CORRECTALL #spelling correction for arguments
+CORRECT_IGNORE_FILE='.*'
 setopt AUTO_CD #Auto cd if cd command not present
- # Use modern completion system
-autoload -Uz compinit
-compinit
-# unalias run-help
-  # autoload run-help
+setopt RM_STAR_WAIT #if you do a 'rm *', Zsh will give you a sanity che
+setopt INTERACTIVECOMMENTS #Comments in the command line
+setopt EXTENDEDGLOB NOMATCH NOTIFY
+setopt NO_CASE_GLOB #Globbing is case-insensitive
