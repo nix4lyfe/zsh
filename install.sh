3a000dc0eb747e9001a2d77436b2473760347641
nix4lyfe <16310251+nix4lyfe@users.noreply.github.com>
2025-03-31 12:23:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd $HOME/zsh
+printf "%s " "Would you like to delete newhostname? <y/N>"
+read prompt
+if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
+then
 rm scripts/newhostname
+fi
 printf "%s " "Would you like to delete cputemp? Note this only works on RaspberryPi <y/N>"
 read prompt
 if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
@@ -17,5 +22,15 @@ sed -i'' -e '/alias laptopmode="$HOME\/scripts\/laptopmode.sh"/d' scripts/aliase
 fi
 cp -R ./{scripts,.zshrc,.zhistory,.config,.local} $HOME/
 cd $HOME/
-curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+chsh -s /bin/zsh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+./node-install.sh
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+printf "%s " "Would you like to delete the downloaded zhs folder? <y/N>"
+read prompt
+if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
+then
 rm -rf zsh
+fi
