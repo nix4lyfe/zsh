5678cda0ac2b6e4f90734f3d91742c59108ce734
nix4lyfe <16310251+nix4lyfe@users.noreply.github.com>
2025-03-31 13:45:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,17 +10,17 @@ printf "%s " "Would you like to delete cputemp? Note this only works on Raspberr
 read prompt
 if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
 then
-rm scripts/cputemp
+rm scripts/cputemp.sh
 sed -i'' -e '/alias cputemp="$HOME\/scripts\/cputemp.sh"/d' scripts/aliases
 fi
 printf "%s " "Would you like to delete laptopmode? <y/N>"
 read prompt
 if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
 then
-rm scripts/laptopmode
+rm scripts/laptopmode.sh
 sed -i'' -e '/alias laptopmode="$HOME\/scripts\/laptopmode.sh"/d' scripts/aliases
 fi
-cp -R ./{scripts,.zshrc,.zhistory,.config,.local} $HOME/
+cp -R ./{scripts,.zshrc,.zhistory,.config,.local,.vimrc} $HOME/
 cd $HOME/
 chsh -s /bin/zsh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
@@ -29,7 +29,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 nvm install 22
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-printf "%s " "Would you like to delete the downloaded zhs folder? <y/N>"
+printf "%s " "Would you like to delete the downloaded zsh folder? <y/N>"
 read prompt
 if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" || "$prompt" == "YES" ]];
 then
